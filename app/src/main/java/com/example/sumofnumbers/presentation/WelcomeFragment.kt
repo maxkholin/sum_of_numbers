f19e91d55b4084af246cd7e2fc1ad18fad9ba3db
madmax <maksimkholin@gmail.com>
2024-10-31 14:51:29 +0300

    add viewBinding

--- app/src/main/java/com/example/sumofnumbers/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/example/sumofnumbers/presentation/WelcomeFragment.kt
@@ -6,15 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.sumofnumbers.R
+import com.example.sumofnumbers.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
+
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding: FragmentWelcomeBinding
+        get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonGo.setOnClickListener {
+
+        }
+    }
 }
